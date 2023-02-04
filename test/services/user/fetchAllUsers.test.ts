2df2aaef96902754738ed97eb3280f3879d0a0b7
Rio <r.arswendo.r@gmail.com>
2023-02-04 19:56:13 +0700

    feat(test): add template for future unit test

--- test/services/user/fetchAllUsers.test.ts
+++ test/services/user/fetchAllUsers.test.ts
@@ -1,17 +1,17 @@
-import fetchAllUsers from '../../../src/services/user/fetchAllUsers';
+import fetchAllUsers from "../../../src/services/user/fetchAllUsers";
 
-import User from '../../../src/db/models/user.model';
+import User from "../../../src/db/models/user.model";
 
 beforeEach(() => {
-    jest.resetAllMocks();
+  jest.resetAllMocks();
 });
 
 describe("Given a function that return a list of every user", () => {
-    it("Returns every user successfully.", async () => {
-        User.findAll = jest.fn().mockReturnValue(['1', '2', '3']);
-        const users = await fetchAllUsers();
+  it("Returns every user successfully.", async () => {
+    User.findAll = jest.fn().mockReturnValue(["1", "2", "3"]);
+    const users = await fetchAllUsers();
 
-        expect(users).toHaveLength(3);
-        expect(User.findAll).toBeCalled();
-    });
-})
+    expect(users).toHaveLength(3);
+    expect(User.findAll).toBeCalled();
+  });
+});
