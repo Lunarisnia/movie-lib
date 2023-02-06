46cb2a267518637e0e5cdaed0ad741b7844dd914
Rio <r.arswendo.r@gmail.com>
2023-02-06 08:55:09 +0700

    fix(schema): refactored path to be dynamic

--- src/gql/schema.ts
+++ src/gql/schema.ts
@@ -1,7 +1,11 @@
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { createSchema } from "graphql-yoga";
+import path from "path";
+
+const typeDefsPath = path.join(__dirname, "**", "*.type.{ts,js}");
+const resolversPath = path.join(__dirname, "**", "resolvers", "*.{ts,js}");
 
 export const schema = createSchema({
-  typeDefs: loadFilesSync("src/gql/**/*.type.ts"),
-  resolvers: loadFilesSync("src/gql/**/resolvers/*.ts"),
+  typeDefs: loadFilesSync(typeDefsPath),
+  resolvers: loadFilesSync(resolversPath),
 });
